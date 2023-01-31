66ab7b5b107c9c9581fc55d95b2a612ed46a2a6a
MaxBulavchyk <maxblk555@gmail.com>
2023-01-31 12:10:53 +0100

    a-trio

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function navToggle() {
   body.classList.toggle("_overflow");
 }
 
+function navClose() {
+  var navToggleNav = document.getElementById("nav");
+  navToggleNav.classList.remove("_active");
+  var html = document.getElementById("html");
+  html.classList.remove("_overflow");
+  var body = document.getElementById("body");
+  body.classList.remove("_overflow");
+}
 
 
 
